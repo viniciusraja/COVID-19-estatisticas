dc9bb17a080ae3c4a3c4837e3859b90d8f4b729d
Vinícius Rajá <viniciusrfl@hotmail.com>
2020-05-07 23:22:16 -0300

    ADD:  assets

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,29 +1,14 @@
-import * as React from 'react';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
+import React from 'react';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
 
-jest.mock('expo', () => ({
-  AppLoading: 'AppLoading',
-}));
-
-jest.mock('../navigation/AppNavigator', () => 'AppNavigator');
-
-describe('App', () => {
-  jest.useFakeTimers();
-
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
-
-  it(`renders the loading screen`, () => {
+describe('<App />', () => {
+  it('renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
   });
-
-  it(`renders the root without loading screen`, () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  
+ 
+  
 });
